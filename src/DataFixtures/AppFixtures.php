1f34556ce5b44b8ca1ba976f2c1a078c8199f688
Habibun Noby <habibun.noby@gmail.com>
2021-02-03 17:39:48 +0600

    feat(product): add default migrations and fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,23 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+/**
+ * Class AppFixtures
+ */
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for ($i = 1; $i <= 20; $i++) {
-            // $product = new Product();
-            // $manager->persist($product);
+        for ($i = 0; $i < 20; $i++) {
+            $product = new Product();
+            $product->setName('product '.$i);
+            $product->setPrice(mt_rand(10, 100));
+            $product->setDescription(bin2hex(random_bytes(10)));
+            $manager->persist($product);
         }
 
         $manager->flush();
