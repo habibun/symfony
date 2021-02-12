e88fe6d21b7f28d8c618b3ea8d61c73f017fca5d
Habibun Noby <habibun.noby@gmail.com>
2021-02-12 11:08:08 +0600

    feat: add nginx config

--- heroku.sh
+++ heroku.sh
@@ -28,3 +28,18 @@ heroku run "php -a"
 # opens up a shell
 heroku run bash
 
+# list add-ons
+heroku addons
+
+# attach add-ons
+heroku addons:create heroku-postgresql:hobby-dev
+
+# open add-ons
+heroku addons:open heroku-postgresql
+
+# config
+heroku config:set APP_SECRET=$(php -r 'echo bin2hex(random_bytes(16));')
+heroku config:set DATABASE_URL="postgres://zrznwhbilurmoo:89e697150c798d4b7faa8d08e95a775bb89be398881b2c818744bf6bab278c07@ec2-35-174-118-71.compute-1.amazonaws.com:5432/dbpiisd34v6bcb"
+
+
+
