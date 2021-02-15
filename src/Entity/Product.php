5e2e4aacafc51d65f3aa058341ac77115c8dd105
Habibun Noby <habibun.noby@gmail.com>
2021-02-15 17:53:15 +0600

    add vich uploader bundle

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,10 +4,15 @@ namespace App\Entity;
 
 use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Vich\UploaderBundle\Entity\File as EmbeddedFile;
 
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
  * @ORM\Table(name="product")
+ * @Vich\Uploadable
  */
 class Product
 {
@@ -38,6 +43,50 @@ class Product
      * @ORM\Column(type="text", length=255, nullable=true)
      */
     private $description;
+
+//    /**
+//     * note: when mapped just a simple property
+//     * @Vich\UploadableField(mapping="products", fileNameProperty="imageName", size="imageSize")
+//     *
+//     * @var File|null
+//     */
+//    private $imageFile;
+
+    /**
+     * @Vich\UploadableField(mapping="products", fileNameProperty="image.name", size="image.size", mimeType="image.mimeType", originalName="image.originalName", dimensions="image.dimensions")
+     *
+     * @var File|null
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Embedded(class="Vich\UploaderBundle\Entity\File")
+     *
+     * @var EmbeddedFile
+     */
+    private $image;
+
+//    /**
+//     * note: when mapped just a simple property
+//     * @ORM\Column(type="string")
+//     *
+//     * @var string|null
+//     */
+//    private $imageName;
+
+//    /**
+//     * note: when mapped just a simple property
+//     *
+//     * @ORM\Column(type="integer")
+//     *
+//     * @var int|null
+//     */
+//    private $imageSize;
+
+    public function __construct()
+    {
+        $this->image = new EmbeddedFile();
+    }
 
     /**
      * @return int|null
@@ -100,6 +149,63 @@ class Product
     public function setDescription(?string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @param File|UploadedFile|null $imageFile
+     * @throws \Exception
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageSize(?int $imageSize): void
+    {
+        $this->imageSize = $imageSize;
+    }
+
+    public function getImageSize(): ?int
+    {
+        return $this->imageSize;
+    }
+
+    /**
+     * @return EmbeddedFile
+     */
+    public function getImage(): ?EmbeddedFile
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param EmbeddedFile $image
+     * @return Product
+     */
+    public function setImage(EmbeddedFile $image): Product
+    {
+        $this->image = $image;
 
         return $this;
     }
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,6 +6,7 @@ use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class ProductType extends AbstractType
 {
@@ -15,6 +16,7 @@ class ProductType extends AbstractType
             ->add('name')
             ->add('price')
             ->add('description')
+            ->add('imageFile', VichFileType::class)
         ;
     }
 
