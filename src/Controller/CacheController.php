d6df1bea2e8f77d82ae4fe3289d3c7f9a0aae7f8
Habibun Noby <habibun.noby@gmail.com>
2021-05-09 22:05:38 +0600

    feat(cache): add memcached

--- src/Controller/CacheController.php
+++ src/Controller/CacheController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Adapter\MemcachedAdapter;
+use Symfony\Component\Cache\Exception\CacheException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +14,12 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * Class CacheController.
  *
- * @Route("/cache", name="cache")
+ * @Route("/cache")
  */
 class CacheController extends AbstractController
 {
     /**
-     * @Route("/file", name="cache")
+     * @Route("/file", name="cache.file")
      */
     public function fileAction(): Response
     {
@@ -38,6 +41,30 @@ class CacheController extends AbstractController
         return new JsonResponse([
             'data' => $cachedData->get(),
             'hit' => $cachedData->isHit(),
+        ]);
+    }
+
+    /**
+     * @Route("/memcached", name="cache.memcached")
+     *
+     * @throws CacheException|InvalidArgumentException
+     */
+    public function memcachedAction(): Response
+    {
+        $client = MemcachedAdapter::createConnection('memcached://localhost');
+        $cache = new MemcachedAdapter($client);
+        $cacheKey = md5('123');
+        $cachedItem = $cache->getItem($cacheKey);
+
+        if (false === $cachedItem->isHit()) {
+            $cachedItem->set($cacheKey, 'some value');
+            $cache->save($cachedItem);
+        }
+
+        return $this->render('cache/memcached.html.twig', [
+            'cache' => [
+                'hit' => $cachedItem->isHit(),
+            ],
         ]);
     }
 }
